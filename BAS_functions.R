e4abfac43e6985503e3c8ef0a6da45a79cb18980
Mark Greenaway <certifiedwaif@gmail.com>
2019-06-17 05:19:42 +1000

    Add another version of BAS_functions.R

---
+++ BAS_functions.R
@@ -0,0 +1,24 @@
+
+
+LogBF_ZS_Laplace <- function(R2, n, p) {
+      M = length(R2)
+      res <- R_LogBF_ZS_Laplace(
+              r2curr=R2, n=as.integer(n), dim=as.integer(p), nmodels=as.integer(M), logmarg=as.double(rep(0,M)))
+      return(res)
+}
+
+
+LogBF_ZS_QUAD <- function(R2, n, p) {
+      M = length(R2)
+      res <- R_LogBF_ZS_QUAD(
+              r2curr=R2, n=as.integer(n), dim=as.integer(p), nmodels=as.integer(M), logmarg=as.double(rep(0,M)))
+      return(res)
+}
+
+LogBF_GonN_Laplace <- function(R2, n, p, alpha) {
+      M = length(R2)
+      res <- R_LogBF_Hg_null_vect(
+              r2curr=R2, n=as.integer(n), dim=as.integer(p), nmodels=as.integer(M), 
+              logmarg=as.double(rep(0,M)), alpha=as.double(alpha), gpower=0)
+      return(res)
+}
